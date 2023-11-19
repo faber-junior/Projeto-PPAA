7b5ca4f4b95f243b19944d8e3af6518ee57ac9aa
Fabricio Pereira da Silva Junior <fabricio.junior1903@gmail.com>
2023-11-19 13:48:49 -0300

    bubble_sort atualizado

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,15 +2,9 @@ lista = [12, 25, 10, 98, 26, 2, 1, 15]
 
 tam_lista = len(lista)
 
-troca = False
-
 for i in range(tam_lista):
-    troca = False
     for n in range(tam_lista - 1):
         if lista[n] > lista[n + 1]:
             lista[n], lista[n + 1] = lista[n + 1], lista[n]
-            troca = True
         print(lista)
-    if troca == False:
-        break
     print()
